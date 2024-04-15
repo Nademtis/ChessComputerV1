aeebece016a23ce25cd280b0720bd777a197c62d
Nademtis <mathias.99.madsen@gmail.com>
2024-04-15 10:40:56 +0200

    draw board added

--- src/Board.java
+++ src/Board.java
@@ -19,5 +19,45 @@ public class Board {
 
     //En måde på at tjekke om et træk er lovligt
 
+    private static String getPieceSymbol(char piece) {
+        switch (piece) {
+            case 'r':
+                return "♜\t";
+            case 'n':
+                return "♞\t";
+            case 'b':
+                return "♝\t";
+            case 'q':
+                return "♛\t";
+            case 'k':
+                return "♚\t";
+            case 'p':
+                return "♟\t";
+            case 'R':
+                return "♖\t";
+            case 'N':
+                return "♘\t";
+            case 'B':
+                return "♗\t";
+            case 'Q':
+                return "♕\t";
+            case 'K':
+                return "♔\t";
+            case 'P':
+                return "♙\t";
+            default:
+                return "\t"; // Two spaces for empty cells
+        }
+    }
 
+    public static void drawBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(8 - i + "\t");
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(getPieceSymbol(board[i][j]));
+            }
+            System.out.println();
+        }
+        System.out.println("    a  b  c d  e f  g  h");
+    }
 }
