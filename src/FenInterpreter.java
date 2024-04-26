0b4091ad6778de13b4e1830de9a02fc90432b041
MatDat <math03t7@stud.kea.dk>
2024-04-26 11:44:19 +0200

    Mailund: FenGenerator and FenInterpreter made

--- src/FenInterpreter.java
+++ src/FenInterpreter.java
@@ -1,2 +1,37 @@
 public class FenInterpreter {
+    public static void main(String[] args) {
+        String fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        char[][] board = interpretFEN(fenString);
+        printBoard(board);
+    }
+
+    public static char[][] interpretFEN(String fenString) {
+        String[] parts = fenString.split(" ");
+        String[] rows = parts[0].split("/");
+        char[][] board = new char[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            int index = 0;
+            for (int j = 0; j < rows[i].length(); j++) {
+                char c = rows[i].charAt(j);
+                if (Character.isDigit(c)) {
+                    index += Character.getNumericValue(c);
+                } else {
+                    board[i][index] = c;
+                    index++;
+                }
+            }
+        }
+
+        return board;
+    }
+
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
