31dfb0dcc601282e22b5803c8e2f7c4c3cd15363
simonj1704 <simo915m@stud.kea.dk>
2024-05-24 09:31:44 +0200

    Made Promotion work for the user

--- src/Board.java
+++ src/Board.java
@@ -70,7 +70,6 @@ public class Board {
                     exit = true;
                     break;
                 }
-                computerMove(false);
 
             } else {
                 computerMoveIterative(true);
@@ -249,6 +248,7 @@ public class Board {
     public boolean makeMove(String move, boolean isWhiteTurn) {
         //todo change how a move is made, idea: move = "pawn e4" where you check what piece can move to that position
         //todo more checks for whether a move is valid
+        try{
         int fromChar = Character.getNumericValue(move.charAt(0)) - 10;
         int fromNum = 9 - Character.getNumericValue(move.charAt(1)) - 1;
         int toChar = Character.getNumericValue(move.charAt(4)) - 10;
@@ -256,11 +256,49 @@ public class Board {
 
         char piece = board[fromNum][fromChar];
 
+        if(piece == ' '){
+            System.out.println("Invalid move: No piece at that position.");
+            return false;
+        }
+
         if (isWhiteTurn && Character.isLowerCase(piece) ||
                 !isWhiteTurn && Character.isUpperCase(piece)) {
             System.out.println("Invalid move: Cannot move opponent's piece.");
             return false;
         } else {
+            if(piece == 'P'){
+                if(toNum == 0){
+                    System.out.println("Promotion! Enter piece to promote to: (Q, R, B, N)");
+                    Scanner in = new Scanner(System.in);
+                    String promotionPiece = in.nextLine();
+                    if(!promotionPiece.equalsIgnoreCase("Q") && !promotionPiece.equalsIgnoreCase("R") &&
+                            !promotionPiece.equalsIgnoreCase("B") && !promotionPiece.equalsIgnoreCase("N")){
+                        System.out.println("Invalid promotion piece. Please enter a valid piece: (Q, R, B, N)");
+                        return false;
+                    }
+                    board[fromNum][fromChar] = ' ';
+                    String promotion = promotionPiece.toUpperCase();
+                    board[toNum][toChar] = promotion.charAt(0);
+                    return true;
+                }
+            }
+
+            if(piece == 'p'){
+                if(toNum == 7){
+                    System.out.println("Promotion! Enter piece to promote to: (q, r, b, n)");
+                    Scanner in = new Scanner(System.in);
+                    String promotionPiece = in.nextLine();
+                    if(!promotionPiece.equalsIgnoreCase("q") && !promotionPiece.equalsIgnoreCase("r") &&
+                            !promotionPiece.equalsIgnoreCase("b") && !promotionPiece.equalsIgnoreCase("n")){
+                        System.out.println("Invalid promotion piece. Please enter a valid piece: (q, r, b, n)");
+                        return false;
+                    }
+                    board[fromNum][fromChar] = ' ';
+                    String promotion = promotionPiece.toLowerCase();
+                    board[toNum][toChar] = promotion.charAt(0);
+                    return true;
+                }
+            }
             board[fromNum][fromChar] = ' ';
             board[toNum][toChar] = piece;
 
@@ -271,6 +309,9 @@ public class Board {
             }
 
             return true;
+        }
+        } catch (Exception e){
+            return false;
         }
     }
 
